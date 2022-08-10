a49a517a3912f2d5cb8b2904b01d29bca7a6ddc4
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-10 14:21:29 -0400

    Tests Passing

--- src/components/CharacterDetail/CharacterDetail.test.jsx
+++ src/components/CharacterDetail/CharacterDetail.test.jsx
@@ -6,7 +6,25 @@ describe('CharacterDetail component', () => {
   afterEach(() => cleanup());
   it('renders CharacterDetail', () => {
     const { asFragment } = render(
-      <CharacterDetail name="Luke Skywalker" birth_year="yo mama"/>
+      <CharacterDetail 
+        name="Luke Skywalker"
+        hair_color="blond"
+        birth_year= "19BBY"
+        films={[
+          'https://swapi.dev/api/films/1/',
+          'https://swapi.dev/api/films/2/',
+          'https://swapi.dev/api/films/3/',
+          'https://swapi.dev/api/films/6/'
+        ]}
+        vehicles={[
+          'https://swapi.dev/api/vehicles/14/',
+          'https://swapi.dev/api/vehicles/30/'
+        ]}
+        starships={[
+          'https://swapi.dev/api/starships/12/',
+          'https://swapi.dev/api/starships/22/'
+        ]}
+      />
     );
     expect(asFragment()).toMatchSnapshot();
   });
