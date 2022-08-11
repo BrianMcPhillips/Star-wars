6c2b2a5b929eb2c612502e3bb36e056172ef65d5
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-11 13:52:48 -0400

    Character detail routing working with id

--- src/containers/CharacterDetailPage/CharacterDetailPage.jsx
+++ src/containers/CharacterDetailPage/CharacterDetailPage.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import CharacterDetail from '../../components/CharacterDetail/CharacterDetail';
 
 const CharacterDetailPage = () => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState({});
 
+  const { id } = useParams();
+
   useEffect(() => {
-    fetch('https://swapi.dev/api/people/1')
+    fetch(`https://swapi.dev/api/people/${id}`)
       .then(res => res.json())
       .then(data => setData(data))
       .finally(() => setLoading(false));
