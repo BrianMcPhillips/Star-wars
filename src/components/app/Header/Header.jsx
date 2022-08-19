3e41c41e20160a1c1aa01abb3861eac947693be1
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-19 19:58:42 -0400

    Header Styling

--- src/components/app/Header/Header.jsx
+++ src/components/app/Header/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
 
 const Header = () => (
-  <header>
-    <div>
-      <h1>Star Wars</h1>
-    </div>
+  <header className={styles.header}>
+    <h1>Star Wars</h1>
     <div>
       <Link to={'/'}>
         Home
