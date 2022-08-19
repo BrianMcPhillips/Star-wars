d3c9a490477e7e0fc6f478f2c0f6827142451643
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-18 21:21:21 -0400

    Loading Gif

---
+++ src/components/app/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => (
+  <div>
+    <img 
+      src="https://i.gifer.com/origin/52/521cf8cd643fa3b98a4b0a57c2f949c2.gif" 
+      alt="Star Wars" />
+  </div>
+);
+
+export default Loading;
