616e58412de7e6f4d52fcfb4a5b2e60f23590d09
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-19 21:15:55 -0400

    Search page functions and state

--- src/containers/search/SearchPage.jsx
+++ src/containers/search/SearchPage.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchPage = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [searchBy, setSearchBy] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
+  const handleSearchBy = ({ target }) => setSearchBy(target.value);
+  const handleSearchTerm = ({ target }) => setSearchTerm(target.velue);
+
+   
   return (
-    
+
   );
 };
 
