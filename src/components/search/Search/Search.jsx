063c8b64b3d5fc6c3f813971d410b938156ed8c3
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-20 21:26:50 -0400

    Search bar

--- src/components/search/Search/Search.jsx
+++ src/components/search/Search/Search.jsx
@@ -3,9 +3,29 @@ import PropTypes from 'prop-types';
 import { terms } from '../../../assets/data';
 
 const Search = ({
-
+  searchBy,
+  searchTerm,
+  handleSearchBy,
+  handleSearchTerm
 }) => (
-
+  <div>
+    <h3>Search</h3>
+    <input onChange={handleSearchTerm} type="text" value={searchTerm}/>
+    <select onChange={handleSearchBy} value={searchBy}>
+      {
+        terms.map((option, i) => 
+          <option key={option + i} value={option}>{option}</option>
+        )
+      }
+    </select>
+  </div>
 );
+
+Search.propTypes = {
+  searchBy: PropTypes.string.isRequired,
+  searchTerm: PropTypes.string.isRequired,
+  handleSearchBy: PropTypes.func.isRequired,
+  handleSearchTerm: PropTypes.func.isRequired
+};
 
 export default Search;
